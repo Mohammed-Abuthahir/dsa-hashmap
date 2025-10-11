c9217e4953363f22a95c0155c14edaac11df96d4
Mohammed-Abuthahir <mohammedabuthahir214@gmail.com>
2025-10-11 09:08:47 +0530

    DSA

---
+++ hashmap/countfrequency.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
+class countfrequency{
+    public static void countfrequency(int[] nums){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+            }
+            else{
+                map.put(nums[i],1);
+            }
+        }
+        System.out.println(map);
+    }
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the Size :");
+        int n = scan.nextInt();
+        System.out.println("Enter the Arrays :");
+        int[] nums = new int[n];
+        for(int i = 0;i<nums.length;i++){
+            nums[i] = scan.nextInt();
+        }
+        countfrequency(nums);
+      
+    }
+
+}
---
+++ hashmap/singlefrequency.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
+class singlefrequency{
+    public static int frequency(int[] nums){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+            }
+            else{
+                map.put(nums[i],1);
+            }
+        }
+        for(int key : map.keySet()){
+            if(map.get(key)==1){
+                return key;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the Size :");
+        int n = scan.nextInt();
+        System.out.println("Enter the Array :");
+        int[] nums = new int[n];
+        for(int i = 0;i<nums.length;i++){
+            nums[i] = scan.nextInt();
+        }
+        int result = frequency(nums);
+        System.out.println("Single Frequency :"+result);
+    }
+}
